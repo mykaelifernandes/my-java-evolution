be9065e561527ff78654c634153af5134d0300a6
Mykaeli fernandes <88605247+mykaelifernandes@users.noreply.github.com>
2022-10-07 20:38:32 -0300

    Add files via upload

---
+++ Dinamica.java
@@ -0,0 +1,18 @@
+public class Dinamica {
+    public static void main(String[] args) {
+
+
+        int valor1 = 0;
+        int valor2 = 12;
+        int resultado = valor1 + valor2;
+
+        if (valor1<=0)
+            System.out.println("Não pode ser menor que zero!");
+        else if (valor2>10)
+            System.out.println("Não pode ser maior que dez!");
+        else
+            System.out.println("O resultado é " + resultado);
+
+
+    }
+}
